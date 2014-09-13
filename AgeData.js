e9dfdc969f891932a6be531b7f0af32c92dfb1a1
noob <carltojord@myvuw.ac.nz>
2014-09-13 22:44:43 +0000

    age data initial template

---
+++ AgeData.js
@@ -0,0 +1,5 @@
+var WellingtonAges2006 = [{AgeGroup: "0-4years", Total: 30378},
+                          {AgeGroup: "5-9years", Total: 30420}];
+
+var WellingtonAges2013 = [{AgeGroup: "0-4years", Total: 31350},
+                          {AgeGroup: "5-9years", Total: 30600}];
