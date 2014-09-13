ca52ea88996c0286c5fbef54cd6bed682857c817
noob <carltojord@myvuw.ac.nz>
2014-09-13 06:19:55 +0000

    stuff

--- PopulationData.js
+++ PopulationData.js
@@ -1,19 +1,59 @@
-var files = ["Pop info.csv", "Comm info.csv"];
-var popFile = files[0];
-var years = popFile.split(/\r\n|\n/);
+var info2001 = [{Northland:144400},
+                {Auckland:1218300},
+                {Waikato:368400},
+                {BayOfPlenty:246900},
+                {Gisborne:45500},
+                {HawkesBay:147300},
+                {Taranaki:105700},
+                {ManawatuWanganui:227500},
+                {Wellington:440200},
+                {Tasman:42400},
+                {Nelson:42900},
+                {Marlborough:40700},
+                {WestCoast:31100},
+                {Canterbury:496700},
+                {Otago:188300},
+                {Southland:93300},
+                {NorthIsland:2944300},
+                {SouthIsland:935400},
+                {NewZealand:3880500}];
 
-var 2001info = [];
-var 2006info = [];
-var 2013info = [];
+var info2006 = [{Northland:152700},
+                {Auckland:1373000},
+                {Waikato:393200},
+                {BayOfPlenty:265300},
+                {Gisborne:46000},
+                {HawkesBay:152100},
+                {Taranaki:107300},
+                {ManawatuWanganui:229400},
+                {Wellington:466300},
+                {Tasman:45800},
+                {Nelson:44300},
+                {Marlborough:43600},
+                {WestCoast:32100},
+                {Canterbury:540000},
+                {Otago:199800},
+                {Southland:93200},
+                {NorthIsland:3185100},
+                {SouthIsland:998800},
+                {NewZealand:4184600}];
 
-for(var year = 0; year < years.length(); year++){
- 
-    var dataPoints = year.split(',');
-    year = dataPoints[0];
-    
-    for(var dataPoint = 1; dataPoint < popFile.length; dataPoint = dataPoint+2){
-        if(year == 0){var 2001info.push(dataPoints[dataPoint]: [dataPoint+1]);}
-        else if (year == 1){var 2006info.push(dataPoints[dataPoint]: [dataPoint+1]);}
-        else if (year == 2){var 2013info.push(dataPoints[dataPoint]: [dataPoint+1]);}
-    }
-}
+var info2013 = [{Northland:158700},
+                {Auckland:1529400},
+                {Waikato:418500},
+                {BayOfPlenty:278000},
+                {Gisborne:46700},
+                {HawkesBay:155000},
+                {Taranaki:110600},
+                {ManawatuWanganui:232700},
+                {Wellington:492500},
+                {Tasman:48600},
+                {Nelson:46800},
+                {Marlborough:45900},
+                {WestCoast:32700},
+                {Canterbury:566100},
+                {Otago:213300},
+                {Southland:94800},
+                {NorthIsland:3422200},
+                {SouthIsland:1048300},
+                {NewZealand:4471100}];
