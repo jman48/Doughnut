db09bc1850890e8b959c54b7111c50e29fea9e45
daphnesiyuan <daphnewangnz@gmail.com>
2014-09-13 23:37:35 +0000

    Filled in the data for age and gender structures

--- AgeData.js
+++ AgeData.js
@@ -1,5 +1,43 @@
-var WellingtonAges2006 = [{AgeGroup: "0-4years", Total: 30378},
-                          {AgeGroup: "5-9years", Total: 30420}];
+var WellingtonAges2006 = [{AgeGroup: "0-4years", Total: 30378, Males: 15438, Females:14940},
+                          {AgeGroup: "5-9years", Total: 30420, Males: 15549, Females: 14874},
+                          {AgeGroup: "10-14years", Total: 31647, Males: 16086, Females: 15561},
+                          {AgeGroup: "15-19years", Total: 32874, Males: 16392, Females: 16485},
+                          {AgeGroup: "20-24years", Total: 32913, Males: 15885, Females: 17031},
+                          {AgeGroup: "25-29years", Total: 29964, Males: 14508, Females: 15456},
+                          {AgeGroup: "30-34years", Total: 33870, Males: 15966, Females: 17907},
+                          {AgeGroup: "35-39years", Total: 32178, Males: 16986, Females: 18699},
+                          {AgeGroup: "40-44years", Total: 35589, Males: 17178, Females: 18411},
+                          {AgeGroup: "45-49years", Total: 32409, Males: 15762, Females: 16647},
+                          {AgeGroup: "50-54years", Total: 27378, Males: 13359, Females: 14016},
+                          {AgeGroup: "55-59years", Total: 25374, Males: 12570, Females: 12804},
+                          {AgeGroup: "60-64years", Total: 19047, Males: 9327, Females: 9723},
+                          {AgeGroup: "65-69years", Total: 15465, Males: 7416, Females: 8049},
+                          {AgeGroup: "70-74years", Total: 12189, Males: 5775, Females: 6417},
+                          {AgeGroup: "75-79years", Total: 10380, Males: 4683, Females: 5697},
+                          {AgeGroup: "80-84years", Total: 7527, Males: 2991, Females: 4536},
+                          {AgeGroup: "85andover", Total: 5847, Males: 1785, Females: 4059}
+                         ];
 
-var WellingtonAges2013 = [{AgeGroup: "0-4years", Total: 31350},
-                          {AgeGroup: "5-9years", Total: 30600}];
+var WellingtonAges2013 = [{AgeGroup: "1-4years", Total: 31350, Males: 15903, Females: 15450},
+                          {AgeGroup: "5-9years", Total: 30600, Males: 15495, Females: 15108},
+                          {AgeGroup: "10-14years", Total: 30051, Males: 15324, Females: 14730},
+                          {AgeGroup: "15-19years", Total: 32892, Males: 16434, Females: 16458},
+                          {AgeGroup: "20-24years", Total: 35412, Males: 17100, Females: 18312},
+                          {AgeGroup: "25-29years", Total: 30429, Males: 14559, Females: 15873},
+                          {AgeGroup: "30-34years", Total: 31092, Males: 14865, Females: 16227},
+                          {AgeGroup: "35-39years", Total: 32178, Males: 15312, Females: 16866},
+                          {AgeGroup: "40-44years", Total: 35628, Males: 16746, Females: 18882},
+                          {AgeGroup: "45-49years", Total: 34359, Males: 16482, Females: 17877},
+                          {AgeGroup: "50-54years", Total: 33129, Males: 15903, Females: 17226},
+                          {AgeGroup: "55-59years", Total: 27555, Males: 13335, Females: 14220},
+                          {AgeGroup: "60-64years", Total: 24369, Males: 11907, Females: 12465},
+                          {AgeGroup: "65-69years", Total: 20262, Males: 9786, Females: 10476},
+                          {AgeGroup: "70-74years", Total: 15294, Males: 7248, Females: 8043},
+                          {AgeGroup: "75-79years", Total: 10800, Males: 4902, Females: 5880},
+                          {AgeGroup: "80-84years", Total: 8382, Males: 3567, Females: 4815},
+                          {AgeGroup: "85andover", Total: 7530, Males: 2679, Females: 4854},
+                                    
+                         
+                         
+                         
+                         ];
