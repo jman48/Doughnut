1d704177066bd8d2612e459de238acead55a30e1
jman48 <j.armstrong484@gmail.com>
2014-09-14 04:36:20 +0000

    Final

---
+++ site/dist/modifieddnut.js
@@ -0,0 +1,329 @@
+ data = [
+       
+{
+    "Year": "2001",
+    "Region": "Hawke's Bay",
+    "Population": "147300"
+},
+{
+    "Year": "2001",
+    "Region": "Taranaki",
+    "Population": "105700"
+},
+{
+    "Year": "2001",
+    "Region": "Manawatu-Wanganui",
+    "Population": "227500"
+},
+{
+    "Year": "2001",
+    "Region": "Wellington",
+    "Population": "440200"
+},
+{
+    "Year": "2001",
+    "Region": "Tasman",
+    "Population": "42400"
+},
+{
+    "Year": "2001",
+    "Region": "Nelson",
+    "Population": "42900"
+},
+{
+    "Year": "2001",
+    "Region": "Marlborough",
+    "Population": "40700"
+},
+{
+    "Year": "2001",
+    "Region": "West Coast",
+    "Population": "31100"
+},
+{
+    "Year": "2001",
+    "Region": "Canterbury",
+    "Population": "496700"
+},
+{
+    "Year": "2001",
+    "Region": "Otago",
+    "Population": "188300"
+},
+{
+    "Year": "2001",
+    "Region": "Southland",
+    "Population": "93300"
+},
+{
+    "Year": "2006",
+    "Region": "Northland",
+    "Population": "152700"
+},
+{
+    "Year": "2006",
+    "Region": "Auckland",
+    "Population": "1373000"
+},
+{
+    "Year": "2006",
+    "Region": "Waikato",
+    "Population": "393200"
+},
+{
+    "Year": "2006",
+    "Region": "Bay of Plenty",
+    "Population": "265300"
+},
+{
+    "Year": "2006",
+    "Region": "Gisborne",
+    "Population": "46000"
+},
+{
+    "Year": "2006",
+    "Region": "Hawke's Bay",
+    "Population": "152100"
+},
+{
+    "Year": "2006",
+    "Region": "Taranaki",
+    "Population": "107300"
+},
+{
+    "Year": "2006",
+    "Region": "Manawatu-Wanganui",
+    "Population": "229400"
+},
+{
+    "Year": "2006",
+    "Region": "Wellington",
+    "Population": "466300"
+},
+{
+    "Year": "2006",
+    "Region": "Tasman",
+    "Population": "45800"
+},
+{
+    "Year": "2006",
+    "Region": "Nelson",
+    "Population": "44300"
+},
+{
+    "Year": "2006",
+    "Region": "Marlborough",
+    "Population": "43600"
+},
+{
+    "Year": "2006",
+    "Region": "West Coast",
+    "Population": "32100"
+},
+{
+    "Year": "2006",
+    "Region": "Canterbury",
+    "Population": "540000"
+},
+{
+    "Year": "2006",
+    "Region": "Otago",
+    "Population": "199800"
+},
+{
+    "Year": "2006",
+    "Region": "Southland",
+    "Population": "93200"
+},
+{
+    "Year": "2013",
+    "Region": "Northland",
+    "Population": "158700"
+},
+{
+    "Year": "2013",
+    "Region": "Auckland",
+    "Population": "1529400"
+},
+{
+    "Year": "2013",
+    "Region": "Waikato",
+    "Population": "418500"
+},
+{
+    "Year": "2013",
+    "Region": "Bay of Plenty",
+    "Population": "278000"
+},
+{
+    "Year": "2013",
+    "Region": "Gisborne",
+    "Population": "46700"
+},
+{
+    "Year": "2013",
+    "Region": "Hawke's Bay",
+    "Population": "155000"
+},
+{
+    "Year": "2013",
+    "Region": "Taranaki",
+    "Population": "110600"
+},
+{
+    "Year": "2013",
+    "Region": "Manawatu-Wanganui",
+    "Population": "232700"
+},
+{
+    "Year": "2013",
+    "Region": "Wellington",
+    "Population": "492500"
+},
+{
+    "Year": "2013",
+    "Region": "Tasman",
+    "Population": "48600"
+},
+{
+    "Year": "2013",
+    "Region": "Nelson",
+    "Population": "46800"
+}];
+
+data2 = [
+    {
+        "year": "2001",
+        "data": [
+            {
+                "type": "no access",
+                "value": 46815
+            },
+            {
+                "type": "mobile",
+                "value": 0
+            },
+            {
+                "type": "telephone",
+                "value": 1240830
+            },
+            {
+                "type": "fax",
+                "value": 325557
+            },
+            {
+                "type": "internet",
+                "value": 482361
+            }
+        ]
+    },
+    {
+        "year": "2006",
+        "data": [
+            {
+                "type": "no access",
+                "value": 0
+            },
+            {
+                "type": "mobile",
+                "value": 1034529
+            },
+            {
+                "type": "telephone",
+                "value": 1277325
+            },
+            {
+                "type": "fax",
+                "value": 362040
+            },
+            {
+                "type": "internet",
+                "value": 843735
+            }
+        ]
+    },
+    {
+        "year": "2013",
+        "data": [
+            {
+                "type": "no access",
+                "value": 24132
+            },
+            {
+                "type": "mobile",
+                "value": 1230174
+            },
+            {
+                "type": "telephone",
+                "value": 1257195
+            },
+            {
+                "type": "fax",
+                "value": 214554
+            },
+            {
+                "type": "internet",
+                "value": 1128738
+            }
+        ]
+    }
+]
+
+var thickness = 30;
+    var width = 500;
+    var height = 500;
+    var color = d3.scale.category20();
+    var testsvg;
+    var svg = null;
+
+function drawPie(data, radius) {  
+
+   
+    if(svg == null) {
+	    svgDonut = d3.select("#donught")
+	    .append("svg")
+	    .attr("width", width)
+	    .attr("height", height);
+	    svg = svgDonut;
+	}
+    
+    var group = svgDonut.append("g")
+    .attr("transform", "translate(" + width/2 + ", " + (height - 100)/2 + ")");
+    
+    var arc = d3.svg.arc()
+    .innerRadius(radius - thickness)
+    .outerRadius(radius)
+    var pie = d3.layout.pie()
+    .value(function(d) {
+        return parseInt(d.Population);
+        
+    });
+    
+    var arcs = group.selectAll(".arc")
+    .data(pie(data))
+    .enter()
+    .append("g")
+    .attr("class", "arc")
+    .on("mouseover", function(d) {
+        //Get spelling right
+        
+    })
+    .on("mouseout", function() {
+        
+    });
+    
+    arcs.append("path")
+    .attr("d", arc)
+    .attr("id", function(d,i){i;
+    })
+    .attr("fill", function(d, i) {
+    	//console.log(d, i)
+    	console.log(color(d.data.Region));
+        return color(d.data.Region);
+        
+    });
+    
+    //Recursively make a call to this method to draw all inner circles until no teams left
+    //drawPie(teams, svgDonut, (radius - thickness));
+    //drawPie(data2, svgDonut, 170);
+}
