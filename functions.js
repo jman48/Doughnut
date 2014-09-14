b57826733105addde4fc4ddee904593676dfc9b7
daphnesiyuan <daphnewangnz@gmail.com>
2014-09-14 00:09:09 +0000

    More Age data and more funcitons

--- functions.js
+++ functions.js
@@ -1,5 +1,29 @@
-var regions = [{}]
+var regions = [{ID:0, Name: "Northland"},
+               {ID:1, Name: "Auckland"},
+               {ID:2, Name: "Waikato"},
+               {ID:3, Name: "BayOfPlenty"},
+               {ID:4, Name: "Gisborne"},
+               {ID:5, Name: "HawkesBay"},
+               {ID:6, Name: "Taranaki"},
+               {ID:7, Name: "ManawatuWanganui"},
+               {ID:8, Name: "Wellington"},
+               {ID:9, Name: "Tasman"},
+               {ID:10, Name: "Nelson"},
+               {ID:11, Name: "Marlborough"},
+               {ID:12, Name: "WestCoast"},
+               {ID:13, Name: "Canterbury"},
+               {ID:14, Name: "Otago"},
+               {ID:15, Name: "Southland" }
+              ]
 
-function getAccessData(region) {
+function getAccessInto(regionID, year) {
+    var reg = regions
+    
+    if(year==2001){
+        commInfo2001[]
+    }
+}
+
+function getNoAccessInto(regionID, year){
     
 }
