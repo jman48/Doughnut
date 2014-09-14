185ecf8b09a61b23301f69b94a3a3ac52c383417
jman48 <j.armstrong484@gmail.com>
2014-09-14 22:44:35 +0000

    Modified doughnt visuals to show less data and also draw inner donught.

--- site/dist/modifieddnut.js
+++ site/dist/modifieddnut.js
@@ -1,165 +1,6 @@
  data2 = [
        
-{
-    "Year": "2001",
-    "Region": "Hawke's Bay",
-    "Population": "27300"
-},
-{
-    "Year": "2001",
-    "Region": "Taranaki",
-    "Population": "12700"
-},
-{
-    "Year": "2001",
-    "Region": "Manawatu-Wanganui",
-    "Population": "217500"
-},
-{
-    "Year": "2001",
-    "Region": "Wellington",
-    "Population": "540200"
-},
-{
-    "Year": "2001",
-    "Region": "Tasman",
-    "Population": "12400"
-},
-{
-    "Year": "2001",
-    "Region": "Nelson",
-    "Population": "52900"
-},
-{
-    "Year": "2001",
-    "Region": "Marlborough",
-    "Population": "30700"
-},
-{
-    "Year": "2001",
-    "Region": "West Coast",
-    "Population": "51100"
-},
-{
-    "Year": "2001",
-    "Region": "Canterbury",
-    "Population": "46700"
-},
-{
-    "Year": "2001",
-    "Region": "Otago",
-    "Population": "188300"
-},
-{
-    "Year": "2001",
-    "Region": "Southland",
-    "Population": "93300"
-},
-{
-    "Year": "2006",
-    "Region": "Northland",
-    "Population": "152700"
-},
-{
-    "Year": "2006",
-    "Region": "Auckland",
-    "Population": "1373000"
-},
-{
-    "Year": "2006",
-    "Region": "Waikato",
-    "Population": "393200"
-},
-{
-    "Year": "2006",
-    "Region": "Bay of Plenty",
-    "Population": "265300"
-},
-{
-    "Year": "2006",
-    "Region": "Gisborne",
-    "Population": "46000"
-},
-{
-    "Year": "2006",
-    "Region": "Hawke's Bay",
-    "Population": "152100"
-},
-{
-    "Year": "2006",
-    "Region": "Taranaki",
-    "Population": "107300"
-},
-{
-    "Year": "2006",
-    "Region": "Manawatu-Wanganui",
-    "Population": "229400"
-},
-{
-    "Year": "2006",
-    "Region": "Wellington",
-    "Population": "466300"
-},
-{
-    "Year": "2006",
-    "Region": "Tasman",
-    "Population": "45800"
-},
-{
-    "Year": "2006",
-    "Region": "Nelson",
-    "Population": "44300"
-},
-{
-    "Year": "2006",
-    "Region": "Marlborough",
-    "Population": "43600"
-},
-{
-    "Year": "2006",
-    "Region": "West Coast",
-    "Population": "32100"
-},
-{
-    "Year": "2006",
-    "Region": "Canterbury",
-    "Population": "540000"
-},
-{
-    "Year": "2006",
-    "Region": "Otago",
-    "Population": "199800"
-},
-{
-    "Year": "2006",
-    "Region": "Southland",
-    "Population": "93200"
-},
-{
-    "Year": "2013",
-    "Region": "Northland",
-    "Population": "158700"
-},
-{
-    "Year": "2013",
-    "Region": "Auckland",
-    "Population": "1529400"
-},
-{
-    "Year": "2013",
-    "Region": "Waikato",
-    "Population": "418500"
-},
-{
-    "Year": "2013",
-    "Region": "Bay of Plenty",
-    "Population": "278000"
-},
-{
-    "Year": "2013",
-    "Region": "Gisborne",
-    "Population": "46700"
-},
+
 {
     "Year": "2013",
     "Region": "Hawke's Bay",
@@ -192,196 +33,45 @@
 }];
 
  data = [
-       
-{
-    "Year": "2001",
-    "Region": "Hawke's Bay",
-    "Population": "147300"
-},
-{
-    "Year": "2001",
-    "Region": "Taranaki",
-    "Population": "105700"
-},
-{
-    "Year": "2001",
-    "Region": "Manawatu-Wanganui",
-    "Population": "227500"
-},
-{
-    "Year": "2001",
-    "Region": "Wellington",
-    "Population": "440200"
-},
-{
-    "Year": "2001",
-    "Region": "Tasman",
-    "Population": "42400"
-},
-{
-    "Year": "2001",
-    "Region": "Nelson",
-    "Population": "42900"
-},
-{
-    "Year": "2001",
-    "Region": "Marlborough",
-    "Population": "40700"
-},
-{
-    "Year": "2001",
-    "Region": "West Coast",
-    "Population": "31100"
-},
-{
-    "Year": "2001",
-    "Region": "Canterbury",
-    "Population": "496700"
-},
-{
-    "Year": "2001",
-    "Region": "Otago",
-    "Population": "188300"
-},
-{
-    "Year": "2001",
-    "Region": "Southland",
-    "Population": "93300"
-},
-{
-    "Year": "2006",
-    "Region": "Northland",
-    "Population": "152700"
-},
-{
-    "Year": "2006",
-    "Region": "Auckland",
-    "Population": "1373000"
-},
-{
-    "Year": "2006",
-    "Region": "Waikato",
-    "Population": "393200"
-},
-{
-    "Year": "2006",
-    "Region": "Bay of Plenty",
-    "Population": "265300"
-},
-{
-    "Year": "2006",
-    "Region": "Gisborne",
-    "Population": "46000"
-},
-{
-    "Year": "2006",
-    "Region": "Hawke's Bay",
-    "Population": "152100"
-},
-{
-    "Year": "2006",
-    "Region": "Taranaki",
-    "Population": "107300"
-},
-{
-    "Year": "2006",
-    "Region": "Manawatu-Wanganui",
-    "Population": "229400"
-},
-{
-    "Year": "2006",
-    "Region": "Wellington",
-    "Population": "466300"
-},
-{
-    "Year": "2006",
-    "Region": "Tasman",
-    "Population": "45800"
-},
-{
-    "Year": "2006",
-    "Region": "Nelson",
-    "Population": "44300"
-},
-{
-    "Year": "2006",
-    "Region": "Marlborough",
-    "Population": "43600"
-},
-{
-    "Year": "2006",
-    "Region": "West Coast",
-    "Population": "32100"
-},
-{
-    "Year": "2006",
-    "Region": "Canterbury",
-    "Population": "540000"
-},
-{
-    "Year": "2006",
-    "Region": "Otago",
-    "Population": "199800"
-},
-{
-    "Year": "2006",
-    "Region": "Southland",
-    "Population": "93200"
-},
-{
-    "Year": "2013",
-    "Region": "Northland",
-    "Population": "158700"
-},
-{
-    "Year": "2013",
-    "Region": "Auckland",
-    "Population": "1529400"
-},
-{
-    "Year": "2013",
-    "Region": "Waikato",
-    "Population": "418500"
-},
-{
-    "Year": "2013",
-    "Region": "Bay of Plenty",
-    "Population": "278000"
-},
-{
-    "Year": "2013",
-    "Region": "Gisborne",
-    "Population": "46700"
-},
-{
-    "Year": "2013",
-    "Region": "Hawke's Bay",
-    "Population": "155000"
-},
-{
-    "Year": "2013",
-    "Region": "Taranaki",
-    "Population": "110600"
-},
-{
-    "Year": "2013",
-    "Region": "Manawatu-Wanganui",
-    "Population": "232700"
-},
-{
-    "Year": "2013",
-    "Region": "Wellington",
-    "Population": "492500"
-},
-{
+     {
     "Year": "2013",
     "Region": "Tasman",
-    "Population": "48600"
+    "Population": "24"
 },
 {
     "Year": "2013",
     "Region": "Nelson",
-    "Population": "46800"
+    "Population": "46"
+},
+{
+    "Year": "2013",
+    "Region": "Hawke's Bay",
+    "Population": "15"
+},
+{
+    "Year": "2013",
+    "Region": "Taranaki",
+    "Population": "60"
+},
+{
+    "Year": "2013",
+    "Region": "Manawatu-Wanganui",
+    "Population": "23"
+},
+{
+    "Year": "2013",
+    "Region": "Wellington",
+    "Population": "12"
+},
+{
+    "Year": "2013",
+    "Region": "Tasman",
+    "Population": "35"
+},
+{
+    "Year": "2013",
+    "Region": "Nelson",
+    "Population": "6"
 }];
 var thickness = 30;
     var width = 500;
